37ad11800f32e8119610574dde094a7edbf07aea
dovanaite <d.dvarionaite@gmail.com>
2022-04-26 22:20:10 +0300

    registration and validation

--- src/app/directives/email-validator.directive.ts
+++ src/app/directives/email-validator.directive.ts
@@ -7,19 +7,5 @@ import { FormControl, ValidationErrors } from '@angular/forms';
 export class EmailValidatorDirective {
 
   constructor() { }
-  validate(control: FormControl): ValidationErrors | null {
-    let email : string=control.value;
-   
-
-
-    let patern :RegExp= /^[a-z|A-Z]{3}[0-9]{3}$/;
-   
-    if ( ! patern.test(email)){
-      return {'error':'Klaida'};
-    }
-
-    
-   
-    return null;
-  }
+ 
 }
